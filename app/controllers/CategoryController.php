6a9f268427f0f83e3b6898f074236213271ce090
adriaanAKQA <adriaan.scholvinck@akqa.com>
2013-06-24 17:37:00 -0400

    adding dynamic loading of categry based on id

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -4,7 +4,18 @@ class CategoryController extends BaseController {
 
 	public function index()
 	{
-		$this->layout->content = View::make('templates.users');
+		$this->layout->content = View::make('templates.categories');
+	}
+
+	public function showCategory( $id = null )
+	{
+		if (is_null($id)) {
+			$id = 'categories';
+		}
+
+		// retreives the data based on the $id
+
+		$this->layout->content = View::make('templates.category');
 	}
 
 }
