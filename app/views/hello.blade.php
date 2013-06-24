2772ebedf4dd452bdbc32514eed07308d2c59d16
adriaanAKQA <adriaan.scholvinck@akqa.com>
2013-06-24 16:15:46 -0400

    templates for two pages

---
+++ app/views/hello.blade.php
@@ -0,0 +1,4 @@
+@section('content')
+<h1>You have arrived.</h1>
+<p>This is the coolest thing ever</p>
+@stop
---
+++ app/views/store.blade.php
@@ -0,0 +1,4 @@
+@section('content')
+<h1>This is the store</h1>
+<p>Lots of stores!</p>
+@stop
