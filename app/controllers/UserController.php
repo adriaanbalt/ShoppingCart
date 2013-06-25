8e11e6011398deb3cbb5185880de9ec18fcbd012
adriaanAKQA <adriaan.scholvinck@akqa.com>
2013-06-25 16:20:46 -0400

    users controller accepts a wildcard will access a database to retreive user data before injecting it into a template

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -6,4 +6,13 @@ class UserController extends BaseController {
 	{
 		$this->layout->content = View::make('templates.site.users');
 	}
+
+	public function getSpecificUser( $id )
+	{
+		// get data specific to $name
+
+		error_log( $id );
+
+		$this->layout->content = View::make('templates.site.user');
+	}
 }
