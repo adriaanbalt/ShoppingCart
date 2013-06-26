386f53459dc9966a03a51da235fa693fd440c735
adriaanbalt <adriaan@balt.us>
2013-06-26 13:13:31 -0400

    base model class that is extended

---
+++ app/models/Model.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Base Model
+ *
+ * Represents the base fields that are used by several other models.
+ */
+
+class Model extends Eloquent
+{
+	/**
+	 * The title of the content or landing page.
+	 */
+	protected $title;
+
+	/**
+	 * The string that is used to get the details for the content
+	 * from the CMS.
+	 */
+	protected $urlTitle;
+
+	/**
+	 * The type of content model this object represents (EE channel)
+	 */
+	protected $type;
+
+	public function setTitle($title)
+	{
+		$this->title = $title;
+		return $this;
+	}
+
+	public function setUrlTitle($urlTitle)
+	{
+		$this->urlTitle = $urlTitle;
+		return $this;
+	}
+
+	public function setType($type)
+	{
+		$this->type = $type;
+		return $this;
+	}
+
+}
