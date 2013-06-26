b932ff2f3720343ea08f3a57358de390766cb246
adriaanbalt <adriaan@balt.us>
2013-06-26 13:13:21 -0400

    static variables

---
+++ app/config/vars.php
@@ -0,0 +1,14 @@
+<?php
+
+// Main application config variables
+return array(
+
+	// CMS
+	'cms_base_url' => 'http://admin.site.com',
+	'cms_cache_enabled' => false,
+	'cms_cache_ttl' => 60, // 1 minute
+
+	// External Blog URL
+	'blog_url' => 'http://blog.site.com/',
+
+);
