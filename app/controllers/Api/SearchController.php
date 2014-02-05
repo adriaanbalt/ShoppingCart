6cc65f377d1c7926c350fc4a568e3c64ab5d834c
adriaanbalt <adriaan@balt.us>
2014-02-05 14:36:48 -0500

    search controller adding debug

--- app/controllers/Api/SearchController.php
+++ app/controllers/Api/SearchController.php
@@ -2,7 +2,7 @@
 
 /**
  * Api/SearchController is used for the "smart" search throughout the site.
- * it returns and array of items (with type and icon specified) so that the selectize.js plugin 
+ * it returns and array of products (with type and icon specified) so that the selectize.js plugin 
  * can render the search results properly
  **/
 
