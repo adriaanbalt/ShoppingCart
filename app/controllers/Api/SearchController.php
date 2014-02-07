9eea882a1f871fcf5cff677020c3433d6fca019e
adriaanbalt <adriaan@balt.us>
2014-02-07 13:43:08 -0500

    adding the status check to the where eloquent command to retreive a list of data that has been published as opposed to not

--- app/controllers/Api/SearchController.php
+++ app/controllers/Api/SearchController.php
@@ -21,7 +21,7 @@ class ApiSearchController extends BaseController {
 	{
 		// operate on the item passed by reference, adding the url based on slug
 		foreach ($data as $key => & $item) {
-			$item['url'] = url($prefix.'/'.$item['slug']);
+			$item['url'] = url($prefix.'/'.$item['hash']);
 		}
 		return $data;
 	}
@@ -36,32 +36,32 @@ class ApiSearchController extends BaseController {
 		// If the input is empty, return an error response
 		if(!$query && $query == '') return Response::json(array(), 400);
 	
-		$products = Product::where('published', true)
-					->where('name','like','%'.$query.'%')
-					->orderBy('name','asc')
+		$products = ProductModel::where('status', 'like','published')
+					->where('title','like','%'.$query.'%')
+					->orderBy('title','asc')
 					->take(5)
-					->get(array('slug','name','icon'))->toArray();
+					->get(array('title'))->toArray();
 	
-		$categories = Category::where('name','like','%'.$query.'%')
-					->has('products')
-					->take(5)
-					->get(array('slug', 'name'))
-					->toArray();
+		// $categories = ProductCategoryModel::where('title','like','%'.$query.'%')
+		// 			->has('products')
+		// 			->take(5)
+		// 			->get(array('slug', 'title'))
+		// 			->toArray();
 		
 		// Data normalization
-		$categories = $this->appendValue($categories, url('img/icons/category-icon.png'),'icon');
+		// $categories = $this->appendValue($categories, url('img/icons/category-icon.png'),'icon');
 		
 		$products   = $this->appendURL($products, 'products');
-		$categories = $this->appendURL($categories, 'categories');
+		// $categories = $this->appendURL($categories, 'categories');
 		
 		// Add type of data to each item of each set of results
 		$products   = $this->appendValue($products, 'product', 'class');
-		$categories = $this->appendValue($categories, 'category', 'class');
+		// $categories = $this->appendValue($categories, 'category', 'class');
 	
 		// Normalize data
 		// Add type of data to each item of each set of results
 		// Merge all data into one array
-		$data = array_merge($products, $categories);
+		$data = array_merge($products);
 	
 		return Response::json(array(
 			'data'=>$data
